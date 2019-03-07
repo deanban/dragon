32119216f3605e267e81987347d89964671cbd43
deanban <pbp2005@gmail.com>
2019-03-06 22:57:04 -0500

    files organized

---
+++ app/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const app = express();
+
+const GenerationEngine = require('./generation/generationEngine');
+const Engine = new GenerationEngine();
+
+Engine.start();
+
+app.get('/dragon/new', (req, res) => {
+  res.json({ dragon: Engine.generation.newDragon() });
+});
+
+module.exports = app;
+
+// const Generation = require('./generation');
+
+// const generation = new Generation();
+// console.log('generation', generation);
+
+// const merlin = generation.newDragon({ nickname: 'merlin' });
+// console.log('merlin', merlin);
+
+// setTimeout(() => {
+//   const mimar = generation.newDragon();
+//   console.log('mimar', mimar);
+// }, 15);
+
+// const Dragon = require('./dragon');
+
+// const fooey = new Dragon({
+//   birthday: new Date().toLocaleString(),
+//   nickname: 'fooey'
+// });
+// const baloo = new Dragon({
+//   birthday: new Date().toLocaleString(),
+//   nickname: 'baloo'
+// });
+
+// setTimeout(() => {
+//   const merlin = new Dragon();
+//   console.log(merlin);
+// }, 1000);
+
+// console.log(fooey);
+// console.log(baloo);
--- server.js
+++ server.js
@@ -1,50 +1,7 @@
-const express = require('express');
-
-const app = express();
-
-const GenerationEngine = require('./generationEngine');
-const Engine = new GenerationEngine();
-
-Engine.start();
-
-app.get('/dragon/new', (req, res) => {
-  res.json({ dragon: Engine.generation.newDragon() });
-});
+const app = require('./app/index');
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, () =>
   console.log(`***********Server Running on Port ${port}***********`)
 );
-
-// const Generation = require('./generation');
-
-// const generation = new Generation();
-// console.log('generation', generation);
-
-// const merlin = generation.newDragon({ nickname: 'merlin' });
-// console.log('merlin', merlin);
-
-// setTimeout(() => {
-//   const mimar = generation.newDragon();
-//   console.log('mimar', mimar);
-// }, 15);
-
-// const Dragon = require('./dragon');
-
-// const fooey = new Dragon({
-//   birthday: new Date().toLocaleString(),
-//   nickname: 'fooey'
-// });
-// const baloo = new Dragon({
-//   birthday: new Date().toLocaleString(),
-//   nickname: 'baloo'
-// });
-
-// setTimeout(() => {
-//   const merlin = new Dragon();
-//   console.log(merlin);
-// }, 1000);
-
-// console.log(fooey);
-// console.log(baloo);
