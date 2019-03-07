4424061dead3b14d55cf66121df53bfd5ae3837a
deanban <pbp2005@gmail.com>
2019-03-06 22:34:25 -0500

    generation engine added

---
+++ generation.js
@@ -0,0 +1,30 @@
+const { REFRESH_RATE, SECONDS } = require('./config');
+const Dragon = require('./dragon');
+
+class Generation {
+  constructor() {
+    this.expiration = this.calculateDragonExpiration();
+  }
+
+  calculateDragonExpiration() {
+    const refreshRate = REFRESH_RATE * SECONDS;
+
+    const expirationPeriod = Math.floor(Math.random() * (refreshRate / 2));
+
+    const msUntilExpiration =
+      Math.random() < 0.5
+        ? refreshRate - expirationPeriod
+        : refreshRate + expirationPeriod;
+
+    return new Date(Date.now() + msUntilExpiration);
+  }
+
+  newDragon({ birthday, nickname, traits } = {}) {
+    if (Date.now() > this.expiration) {
+      throw new Error(`This dragon expired on ${this.expiration}`);
+    }
+    return new Dragon({ birthday, nickname, traits });
+  }
+}
+
+module.exports = Generation;
--- server.js
+++ server.js
@@ -1,18 +1,41 @@
-const Dragon = require('./dragon');
+const GenerationEngine = require('./generationEngine');
 
-const fooey = new Dragon({
-  birthday: new Date().toLocaleString(),
-  nickname: 'fooey'
-});
-const baloo = new Dragon({
-  birthday: new Date().toLocaleString(),
-  nickname: 'baloo'
-});
+const Engine = new GenerationEngine();
+
+Engine.start();
 
 setTimeout(() => {
-  const merlin = new Dragon();
-  console.log(merlin);
-}, 1000);
+  Engine.stop();
+}, 20000);
 
-console.log(fooey);
-console.log(baloo);
+// const Generation = require('./generation');
+
+// const generation = new Generation();
+// console.log('generation', generation);
+
+// const merlin = generation.newDragon({ nickname: 'merlin' });
+// console.log('merlin', merlin);
+
+// setTimeout(() => {
+//   const mimar = generation.newDragon();
+//   console.log('mimar', mimar);
+// }, 15);
+
+// const Dragon = require('./dragon');
+
+// const fooey = new Dragon({
+//   birthday: new Date().toLocaleString(),
+//   nickname: 'fooey'
+// });
+// const baloo = new Dragon({
+//   birthday: new Date().toLocaleString(),
+//   nickname: 'baloo'
+// });
+
+// setTimeout(() => {
+//   const merlin = new Dragon();
+//   console.log(merlin);
+// }, 1000);
+
+// console.log(fooey);
+// console.log(baloo);
