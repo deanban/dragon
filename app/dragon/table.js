bcb7d532805ba3c5b5cbb0169de33584a7f73956
deanban <pbp2005@gmail.com>
2019-03-24 14:48:03 -0400

    accountDragons auth HOC done

--- app/dragon/table.js
+++ app/dragon/table.js
@@ -63,6 +63,19 @@ class DragonTable {
             );
         });
     }
+
+    static updateDragon({ dragonId, nickname }) {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                'UPDATE dragon SET nickname=$1 WHERE id=$2',
+                [nickname, dragonId],
+                (err, res) => {
+                    if (err) return reject(err);
+                    resolve();
+                }
+            );
+        });
+    }
 }
 
 // DragonTable.getDragon({ dragonId: 1 })
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { Router, Switch, Route } from 'react-router-dom';
+import {
+  Router, Switch, Route, Redirect
+} from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import './App.css';
 
@@ -20,6 +22,16 @@ import AccountDragons from './components/AccountDragons';
 
 const history = createBrowserHistory();
 
+const AuthRouteHOC = (props) => {
+  if (!store.getState().account.loggedIn) {
+    return <Redirect to={{ pathname: '/' }} />;
+  }
+
+  const { component, path } = props;
+
+  return <Route exact path={path} component={component} />;
+};
+
 class App extends Component {
   render() {
     return (
@@ -28,7 +40,10 @@ class App extends Component {
           <Router history={history}>
             <Switch>
               <Route exact path="/" component={Root} />
-              <Route exact path="/account-dragons" component={AccountDragons} />
+              <AuthRouteHOC
+                path="/account-dragons"
+                component={AccountDragons}
+              />
             </Switch>
           </Router>
         </div>
