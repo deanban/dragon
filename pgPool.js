93205bed8b9639a13c8ec20351e24b5df7ca9f40
deanban <pbp2005@gmail.com>
2019-03-28 19:08:42 -0400

    trying heroku

--- pgPool.js
+++ pgPool.js
@@ -4,11 +4,6 @@ const dotenv = require('dotenv');
 const ENV_VARS = dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_DEV_HOST,
-    database: process.env.DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
     connectionString: process.env.DATABASE_URL
 });
 
