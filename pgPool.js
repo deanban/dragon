df955f1937728094558e638d07bff30c9531baf9
deanban <pbp2005@gmail.com>
2019-03-28 18:13:57 -0400

    trying heroku

--- pgPool.js
+++ pgPool.js
@@ -8,7 +8,8 @@ const pool = new Pool({
     host: process.env.DB_DEV_HOST,
     database: process.env.DATABASE,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL
 });
 
 if (ENV_VARS.error) {
